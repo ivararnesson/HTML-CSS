b6ff1288e0af155d06a582241b421eb845263b07
Ivar Arnesson <ivararnesson@Ivars-MacBook-Pro.local>
2023-10-27 21:31:45 +0200

    Menyknappen

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 var menyKnappen = document.getElementById("meny-knapp");
 var meny = document.querySelector(".meny");
 
-menyKnappen.addEventListener("click", function()
-{
-    if (meny.style.display === "none" || meny.style.display === "")
-    {
-        meny.style.display = "none";
-    }
-    else
-    {
-        meny.style.display = "block";
+menyKnappen.addEventListener("click", function() {
+    if (meny.classList.contains("visa")) {
+        meny.classList.remove("visa");
+    } else {
+        meny.classList.add("visa");
     }
 });
