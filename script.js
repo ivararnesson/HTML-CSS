f647d4d2a76583f2725849bc49a4b2faff6f31d2
Ivar Arnesson <ivararnesson@Ivars-MacBook-Pro.local>
2023-10-27 15:13:08 +0200

    la til i koden

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var meny = document.querySelector(".meny");
 
 menyKnappen.addEventListener("click", function()
 {
-    if (meny.style.display === "none")
+    if (meny.style.display === "none" || meny.style.display === "")
     {
         meny.style.display = "block";
     }
